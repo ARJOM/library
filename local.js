570869aa607e96afe1a5f3350788c168fbb3f7c0
ARJOM <38226399+ARJOM@users.noreply.github.com>
2019-05-07 17:01:52 -0300

    Bug de Cadastro Concertado
    Adicionado um onload para "cadastrar" o administrador.

--- local.js
+++ local.js
@@ -1,15 +1,26 @@
-usuarios = []
+usuarios = [];
+
+function carregar(){
+    try {
+        usuarios = getObjectLocalStorage("usuarios");
+    }
+    catch (usuarios){
+        var admin = new Usuario("admin", "admin", "admin");
+        usuarios.push(admin);
+        setObjectLocalStorage("usuarios", usuarios);
+    }
+}
 
 function cadastro() {
     if (typeof (Storage) !== "undefined") {
         var nome = document.getElementById("novo_nome").value;
         var senha = document.getElementById("nova_senha").value;
 
-        var usuario = new Usuario(nome, senha);
+        var usuario = new Usuario(nome, senha, "normal");
+        var usuarios = getObjectLocalStorage("usuarios");
         usuarios.push(usuario);
 
         setObjectLocalStorage("usuarios", usuarios);
-        
 
     } else {
         window.alert("API Web Storage não encontrada");
@@ -25,6 +36,7 @@ function login() {
     for (var i = 0; i < cadastrados.length; i++){
         resultado = cadastrados[i];
         if (resultado.nome == nome && resultado.senha == senha) {
+            setObjectLocalStorage("logado", resultado);
             window.alert("logou");
         }
     };
